539d8925b270b4134cc9c541242591323d2f4917
Sara Harder <harders@oregonstate.edu>
2024-01-23 12:39:11 +0100

    Added a basic list to the Home Screen

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # CS467-Crowd-Sourced-Shopping-App
-OSU Course: CS467
+
 Authors: drososk@oregonstate.edu, giacobbj@oregonstate.edu, tanglon@oregonstate.edu, harders@oregonstate.edu
--- UI/pages/HomeScreen.js
+++ UI/pages/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   SafeAreaView,
+  FlatList,
   ScrollView,
   StatusBar,
   StyleSheet,
@@ -9,10 +10,42 @@ import {
   View,
 } from 'react-native';
 
+// REPLACE this with data pulled from the database
+const list_data = ["tomato sauce", "potatoes", "cherries", "chicken", "bread crumbs"]
+
+
+type ListProps = {
+    item: string;
+};
+
+const ListElement = (props: ListProps) => {
+// text component for one element in the shopping list
+    return (
+        <Text>
+            {props.item}
+        </Text>
+    )
+}
+
+const ShoppingList = () => {
+// list component for the whole shopping list
+    return(
+        <FlatList
+            data={list_data}
+            renderItem = {
+                ({item}) =>
+                <ListElement item={item} />
+            }
+        />
+    )
+}
+
+
 function HomeScreen() {
+// the Home screen itself with its components
   return (
     <SafeAreaView>
-
+        <ShoppingList />
     </SafeAreaView>
   );
 };
