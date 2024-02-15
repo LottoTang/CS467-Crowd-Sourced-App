ed813ad14393f3724e587d96c1f82f34e5b62691
LottoTang <tanglon@oregonstate.edu>
2024-02-15 12:03:24 +0800

    users route updates - 1

--- backend/src/users-controller.js
+++ backend/src/users-controller.js
@@ -2,82 +2,48 @@
 // Topic: Crowded-Sourced Shopping App - Backend (User Controller)
 // Author: Long To Lotto Tang
 // Creation Date: 1/29/2024
-// Remarks: Auth0 Default: Login (users/login); Logout (users/logout)
 
-require('dotenv').config();
 const express = require('express');
 const db = require('./users-model.js');
-const {auth, requiresAuth} = require('express-openid-connect');
 const bodyParser = require('body-parser');
 
 const usersRouter = express.Router();
-const jsonParser = bodyParser.json();
-
-// Utility Functions
-const stringGenerator = numChar => {
-  const characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  var result = '';
-  const charactersLength = characters.length;
-  for (var i = 0; i < numChar; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
-};
-
-const USERS_BASE_URL = 'http://localhost:3000/users';
-
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  baseURL: USERS_BASE_URL,
-  clientID: process.env.AUTH0_CLIENT_ID,
-  issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}`,
-  secret: stringGenerator(20),
-};
-
-usersRouter.use(auth(config));
-
-usersRouter.get('/', async (req, res) => {
-  if (req.oidc.isAuthenticated()) {
-    // Check if new or existing user
-    try {
-      const document = await db.findUserByAuthSub(req.oidc.user.sub);
-      if (!document) {
-        // Tell the frontend to redirect to fill in information page
-        res.status(200).send('Tell frontend to go to information page.');
-      } else {
-        // Existing user, return the data
-        res.status(200).send(document);
-      }
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
-    }
-  } else res.send('Logged Out');
-});
+usersRouter.use(bodyParser.json());
 
 // CREATE: Create a User to database
-usersRouter.post('/', requiresAuth(), jsonParser, async (req, res) => {
-  const userInfo = req.oidc.user;
-  const fullname = req.body.fullname;
-  const username = req.body.username;
-  const city = req.body.city;
-  const state = req.body.state;
-  // Data Validation: req.body entry is not empty
-  if (!fullname || !username || !city || !state) {
-    return res.status(400).send({Error: 'Missing one of the required fields.'});
-  }
+usersRouter.post('/', async (req, res) => {
+  const userAuthSub = req.body.userAuthSub;
   try {
-    const user = await db.createUsers(
-      userInfo.sub,
-      userInfo.email,
-      fullname,
-      username,
-      city,
-      state,
-    );
-    res.status(201).send(user);
+    const isRegistered = await db.findUserByAuthSub(userAuthSub);
+    if (isRegistered) {
+      res.status(409).send({Message: 'Redircting to mainpage.'});
+    } else {
+      const userEmail = req.body.email;
+      const userFirstName = req.body.firstName;
+      const userLastName = req.body.lastName;
+      const userCity = 'Corvallis';
+      const userState = 'OR';
+      // Data validation: firstName and lastName is not empty
+      if (!userFirstName || !userLastName) {
+        return res
+          .status(400)
+          .send({Error: 'Missing one of the required fields.'});
+      }
+      try {
+        const user = await db.createUsers(
+          userAuthSub,
+          userEmail,
+          userFirstName,
+          userLastName,
+          userCity,
+          userState,
+        );
+        res.status(201).send(user);
+      } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+      }
+    }
   } catch (err) {
     console.error(err);
     res.status(500).send('Internal Server Error');
@@ -97,15 +63,14 @@ usersRouter.get('/:_id', async (req, res) => {
 });
 
 // UPDATE: Update User's personal particulars
-usersRouter.patch('/:_id', jsonParser, async (req, res) => {
+usersRouter.patch('/:_id', async (req, res) => {
   const userID = req.params._id;
   try {
     const document = await db.findUserById(userID);
     const update = {};
-    if (req.body.fullname) update.fullname = req.body.fullname;
-    if (req.body.username) update.username = req.body.username;
-    if (req.body.city) update.city = req.body.city;
-    if (req.body.state) update.state = req.body.state;
+    if (req.body.userFirstName) update.userFirstName = req.body.userFirstName;
+    if (req.body.userLastName) update.userLastName = req.body.userLastName;
+    if (req.body.userName) update.userName = req.body.userName;
     try {
       const updateCount = await db.updateUser({_id: userID}, update);
       // return the number of modified item (Expected: 1)
@@ -120,15 +85,16 @@ usersRouter.patch('/:_id', jsonParser, async (req, res) => {
   }
 });
 
-// UPDATE: Update User's shopping_list_item (Temporary Code)
-usersRouter.patch('/shopping-list-item/:_id', jsonParser, async (req, res) => {
+// UPDATE: Update User's shopping_list_item
+usersRouter.patch('/shopping-list-item/:_id', async (req, res) => {
   const userID = req.params._id;
   try {
     const document = await db.findUserById(userID);
     const shoppingListItem = req.body;
     try {
-      const parsedShoppingListItem =
-        await db.parseShoppingListItem(shoppingListItem);
+      const parsedShoppingListItem = await db.parseShoppingListItem(
+        shoppingListItem,
+      );
       // Update shopping_list_item to database
       const update = {};
       update.shopping_list_item = parsedShoppingListItem;
