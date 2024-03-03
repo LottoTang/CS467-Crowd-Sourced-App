6f6f084c111fd7f8e80e1cd3b79d5efe9e09eac0
Sara Harder <harders@oregonstate.edu>
2024-03-03 19:22:28 +0100

    Integrated the clear shopping list func in the ItemsAtStore page

--- UI/pages/ItemsAtStore.js
+++ UI/pages/ItemsAtStore.js
@@ -13,9 +13,12 @@ import { useNavigation } from '@react-navigation/native';
 import { useDispatch, useSelector } from 'react-redux';
 
 // function imports
-import { getShoppingListItemsInStore, getProductInShoppingListDetails } from "../../redux/funtionality/helperFunctions";
-import { viewSelectedItem, deleteItemInShoppingList } from '../../redux/actions/actions.js';
+import { getShoppingListItemsInStore, getProductInShoppingListDetails, removeSelectedItem } from "../../redux/funtionality/helperFunctions";
+import { viewSelectedItem, setUser } from '../../redux/actions/actions.js';
 import { removeItemFromArray } from '../ui_helpers.js';
+
+// data imports
+import { updateShoppingList } from '../../redux/funtionality/postPatchFunctions.js';
 
 // component imports
 import Loading from '../components/LoadingPage.js'
@@ -53,7 +56,9 @@ const ItemComponent = ({item, data}) => {
 function ItemsAtStore({route}) {
 // the View Items at Specified Store screen itself with its components
     const store = route.params.store
+    const user = useSelector((state)=> state.user);
     const shopping_list = useSelector((state)=> state.user.shopping_list_item);
+    const shopping_list_content = useSelector(state => state.shopping_list_content);
 
     const [loading, setLoading] = useState(true);
 
@@ -101,10 +106,13 @@ function ItemsAtStore({route}) {
         }
     }
 
-    const clearSelected = () => {
+    const clearSelected = async () => {
+        let updatedList = shopping_list_content
         for (const product of selected_products){
-
+            updatedList = removeSelectedItem(updatedList, product);
         }
+        const res = await updateShoppingList(user._id, updatedList)
+        dispatch(setUser(res));
         setSelectedItems([])
     };
 
