15bf47fc81dd92c88f5baf4d788e13826b07c301
LottoTang <tanglon@oregonstate.edu>
2024-03-04 01:20:50 +0800

    Updated Users and Stores (substring, date)

--- backend/src/users-model.js
+++ backend/src/users-model.js
@@ -19,6 +19,7 @@ const usersSchema = new mongoose.Schema(
     state: {type: String, required: true},
     shopping_level: {type: Number, required: true},
     feed_item_count: {type: Number, required: true},
+    user_creation_date: {type: Date, default: '2023-03-01'},
   },
   {minimize: false},
 );
@@ -116,6 +117,51 @@ const updateUserShoppingLevel = async _id => {
   return updateFeedItemCount.modifiedCount;
 };
 
+// UPDATE: Lower the shopping_level
+const lowerUserShoppingLevel = async _id => {
+  const document = await Users.findOne({_id: _id});
+  if (document.feed_item_count === 0) {
+    return 0;
+  }
+  const lowerFeedItemCount = await Users.updateOne(
+    {
+      _id: _id,
+    },
+    {
+      $inc: {
+        feed_item_count: -1,
+      },
+    },
+  );
+  const userFeedItemCount = document.feed_item_count - 1;
+  var userShoppingLevel = 0;
+  switch (true) {
+    case userFeedItemCount >= 0 && userFeedItemCount <= 2:
+      userShoppingLevel = 1;
+      break;
+    case userFeedItemCount >= 3 && userFeedItemCount <= 4:
+      userShoppingLevel = 2;
+      break;
+    case userFeedItemCount >= 5 && userFeedItemCount <= 6:
+      userShoppingLevel = 3;
+      break;
+    case userFeedItemCount >= 7 && userFeedItemCount <= 8:
+      userShoppingLevel = 4;
+      break;
+    case userFeedItemCount >= 9:
+      userShoppingLevel = 5;
+      break;
+  }
+  // Update the new shopping level back to database
+  const result = await Users.updateOne(
+    {_id: _id},
+    {shopping_level: userShoppingLevel},
+  );
+
+  // Return the number of FeedItemCount (Expected: 1)
+  return lowerFeedItemCount.modifiedCount;
+};
+
 // UTILITY FUNCTION: Parse shopping_list_item with items.id
 
 // Expected input:  {"Products.name": ["brandA", "brandB"]}
@@ -170,5 +216,6 @@ module.exports = {
   findUserByAuthSub,
   updateUser,
   updateUserShoppingLevel,
+  lowerUserShoppingLevel,
   parseShoppingListItem,
 };
